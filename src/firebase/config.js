dc1a7d00d7428281ff7330f57ca85c97234c2488
Joseph Kniskern <kniskernjoseph@yahoo.com>
2021-01-20 20:08:22 -0500

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnEh3SdcXg9FW_wQi1LJB7-t2oO3cLLGY",
+  authDomain: "forum-9d229.firebaseapp.com",
+  projectId: "forum-9d229",
+  storageBucket: "forum-9d229.appspot.com",
+  messagingSenderId: "684853563537",
+  appId: "1:684853563537:web:9fd93dd3ef46f79fcbaf30",
+};
+
+export default firebaseConfig;
